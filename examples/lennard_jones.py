f2c393f4f1772c70c6c52ef673bfaf0663c54019
Pablo Alcain <pabloalcain@gmail.com>
2015-04-27 18:46:12 -0300

    Lennard-Jones example works with new version

--- examples/lennard_jones.py
+++ examples/lennard_jones.py
@@ -1,24 +1,42 @@
 """
 Example of Lennard-Jones method
 """
-
 import mdsys
+import numpy as np
+from itertools import product
 
 #Set the box
-box = mdsys.Box([[-10, 10], [-10, 10], [-10, 10]])
+L = [0, 5] 
+box = mdsys.Box([L, L, L])
 
-#Create 40 particles
-part = mdsys.Particles(box, N = 40)
+#Start particles instance
+part = mdsys.Particles(box, dim = 3)
 
+#Create 125 particles in a lattice
+nL = 5
+npart = nL**3
+x = np.zeros((npart, 3))
+for (i, j, k) in product(range(nL), range(nL), range(nL)):
+    x[i * nL**2 + j * nL + k, :] = [i, j, k]
+v = 0.5*np.random.rand(npart, 3)
+part.add_particles(npart, x, v)
+    
 #Set interaction
 lennard = lambda r: 4 * (r**(-12.0) - r**(-6.0))
-lj = mdsys.Potential(lennard, 2.5)
-interactions = {}
-interactions[('A', 'A')] = lj
+lj = mdsys.Potential(lennard, 2.5, 5000)
+interactions = {('A', 'A'): lj}
 
 #Create system
-system = mdsys.MDSys(box, part, interactions, 0.01)
+system = mdsys.MDSys(box, part, interactions, 1e-4)
 
-system.run(1000)
-
-
+#Run
+fp = open("thermo.dat", "w")
+print>>fp, "#Potential Energy, Kinetic Energy, Total Energy"
+for i in range(1000):
+    print i
+    system.run(1)
+    system.particles.kinetic_energy()
+    pe = system.particles.potential
+    ke = system.particles.kinetic
+    system.dump()
+    print>>fp, pe, ke, pe + ke
